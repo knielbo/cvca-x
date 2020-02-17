4d568ddebe7c85178ca5e0cbb2078af9a1ab6814
knielbo <kln@cas.au.dk>
2020-02-17 20:11:09 +0100

    add main

--- codebase/gradient_descent.py
+++ codebase/gradient_descent.py
@@ -2,18 +2,20 @@
 """
 Implementing basic gradient descent
 """
+import os
+import argparse
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.datasets import make_blobs
+
 import matplotlib.pyplot as plt
-import numpy as np
-import argparse
 
 
 def sigmoid_activation(x):
     """ sigmoid activation function/scoring function for input x """
     
-    return 1.0 / (1 + np.exp(x))
+    return 1.0 / (1 + np.exp(-x))
 
 def sigmoid_deriv(x):
     """ compute derivate of sigmoid assuming that x has be passed through the sigmoid function """
@@ -50,6 +52,58 @@ def main():
     print("[INFO] training...")
     W = np.random.randn(X.shape[1], 1)
     losses = list()
+
+    # loop for e epochs
+    for epoch in np.arange(0, args["epochs"]):
+        print(epoch)
+        # dot product og X and W and feed through activation function to give model prediction
+        
+        preds = sigmoid_activation(trainX.dot(W))
+
+        # determine error and loss using SSE
+        error = preds - trainY
+        loss = np.sum(error ** 2)
+        losses.append(loss)
+
+        # gradient descent update is the dot product between features X and the error of the sigmoid derivative
+            # of the predictions
+        d = error * sigmoid_deriv(preds)
+        gradient = trainX.T.dot(d)
+
+        # nudge the weight matrix in the negative direction of the gradient
+        W += -args["alpha"] * gradient
+
+        # check for diplay
+        if epoch == 0 or (epoch + 1) % 5 == 0:
+            print("[INFO] epoch={}, loss={:.7f}".format(int(epoch+1), loss))
+        
+    print("[INFO] evaluating...")
+    preds = predict(testX, W)
+    print(classification_report(testY, preds))
+
+    # plot testing
+    plt.style.use("fivethirtyeight")
+    plt.figure()
+    plt.title("$Data$")
+    plt.scatter(testX[:,0],testX[:,1], marker="o",c=testY[:,0], s=30)
+    plt.tight_layout()
+    plt.savefig(os.path.join("..","figures","gradient_descent_data.png"))
+    plt.close()
+
+    # plot loss over time
+    plt.style.use("fivethirtyeight")
+    plt.figure()
+    plt.plot(np.arange(0, args["epochs"]), losses)
+    plt.title("$Training~Loss$")
+    plt.xlabel("$Epoch #$")
+    plt.ylabel("$Loss$")
+    plt.tight_layout()
+    plt.savefig(os.path.join("..","figures","gradient_descent_loss.png"))
+    plt.close()
+
+
+
+
 
 
 if __name__=="__main__":
