3e72a7a7fb8c48c55a41da5012382ca6d051ac4f
knielbo <kln@cas.au.dk>
2020-02-17 20:13:04 +0100

    add linear dog classifier

---
+++ codebase/main.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# example of usage 
+## suppress warnings on call
+start=`date +%s`
+#echo pipeline init
+#while true;do echo -n '>';sleep 1;done &
+
+
+python knn.py --dataset ../datasets/animals
+python linear_example.py
+python gradient_descent.py
+
+
+
+#kill $!; trap 'kill $!' SIGTERM
+#echo
+#echo ':)'
+
+
+
+end=`date +%s`
+runtime=$((end-start))
+echo $runtime
