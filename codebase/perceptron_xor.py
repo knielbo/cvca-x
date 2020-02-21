9feda0dfcf61969853901666ead8e6533a9a4b4a
knielbo <kln@cas.au.dk>
2020-02-21 14:03:11 +0100

    XOR test

---
+++ codebase/perceptron_xor.py
@@ -0,0 +1,28 @@
+#!/home/knielbo/virtenvs/cv/bin/python
+"""
+Train Perceptron class on XOR
+"""
+from kartina.nn import Perceptron
+import numpy as np
+
+def main():
+    # construct dataset
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    y = np.array([[0], [1], [1], [0]])
+
+    # define and train perceptron
+    print("[INFO] training perceptron...")
+    p = Perceptron(X.shape[1], alpha=.1)
+    p.fit(X, y, epochs=20)
+
+    # evaluate
+    print("[INFO] testing perceptron...")
+
+    # loop over data points
+    for (x, target) in zip(X, y):
+        # predict and display
+        pred = p.predict(x)
+        print("[INFO] data={}, ground-truth={}, pred={}".format(x, target[0], pred))
+
+if __name__=="__main__":
+    main()
