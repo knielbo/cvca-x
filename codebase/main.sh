4f98d681d106078e80100c4a54f5769158f0fbe8
knielbo <kln@cas.au.dk>
2020-02-21 14:07:34 +0100

    add perceptron pipeline

--- codebase/main.sh
+++ codebase/main.sh
@@ -6,18 +6,18 @@ start=`date +%s`
 #echo pipeline init
 #while true;do echo -n '>';sleep 1;done &
 
-
 python knn.py --dataset ../datasets/animals
 python linear_example.py
 python gradient_descent.py
 python sgd-py --batch-size 5
-
+python regularization.py --dataset ../datasets/animals
+python perceptron_or.py
+python perceptron_and.py
+python perceptron_xor.py
 
 #kill $!; trap 'kill $!' SIGTERM
 #echo
 #echo ':)'
-
-
 
 end=`date +%s`
 runtime=$((end-start))
