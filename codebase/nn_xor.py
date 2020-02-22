cd7e24dc74327421fb49542adb73cf4fc3bf4b2a
knielbo <kln@cas.au.dk>
2020-02-22 13:26:52 +0100

    add experiment w no hidden layer

--- codebase/nn_xor.py
+++ codebase/nn_xor.py
@@ -12,7 +12,10 @@ def main():
 
     # define and train perceptron
     print("[INFO] training neural network...")
-    nn = NeuralNetwork([2, 2, 1], alpha=.5)
+    #nn = NeuralNetwork([2, 2, 1], alpha=.5)
+    # test without hidden layer/update
+    nn = NeuralNetwork([2, 1], alpha=.5)
+
     nn.fit(X, y, epochs=20000)
 
     # evaluate
@@ -24,5 +27,6 @@ def main():
         pred = nn.predict(x)[0][0]
         step = 1 if pred > .5 else 0
         print("[INFO] data={}, ground-truth={}, pred={:.4f}, step={}".format(x, target[0], pred, step))
+
 if __name__=="__main__":
     main()
