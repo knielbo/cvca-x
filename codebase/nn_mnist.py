c6bd661ae38aadb8652bc8c0c961c6b9b9a85270
knielbo <kln@cas.au.dk>
2020-02-23 10:44:07 +0100

    import dependencies

--- codebase/nn_mnist.py
+++ codebase/nn_mnist.py
@@ -3,3 +3,7 @@
 Train NeuralNetwork instance on mnist (subset of) as baseline
 """
 from kartina.nn import NeuralNetwork
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import datasets
