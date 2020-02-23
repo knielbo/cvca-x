9c101aa38c89b32abcf2be3315520bac6ee34dfe
knielbo <kln@cas.au.dk>
2020-02-23 14:42:14 +0100

    import dependencies

--- codebase/keras_mnist.py
+++ codebase/keras_mnist.py
@@ -2,3 +2,14 @@
 """
 Train feedforward NN on mnist (full) as baseline with Keras & TF
 """
+import numpy as np
+import argparse
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import classification_report
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
+
