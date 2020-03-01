06b73086702aa88d09a1facb5b6be6172728b237
knielbo <kln@cas.au.dk>
2020-03-01 13:18:02 +0100

    add depedencies

---
+++ codebase/convolutions.py
@@ -0,0 +1,19 @@
+#!/home/knielbo/virtenvs/cv/bin/python
+"""
+Implementation of convolutions
+"""
+from skimage.exposure import rescale_intensity
+import numpy as np
+import argparse
+import cv2
+
+
+
+
+
+def main():
+    print("yolo")
+
+
+if __name__=="__main__":
+    main()
