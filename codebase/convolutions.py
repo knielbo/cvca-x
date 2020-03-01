9bf93bdbcda5b404e5b2f0185a23c5c740eb7347
knielbo <kln@cas.au.dk>
2020-03-01 14:30:23 +0100

    loop over kernel bank

--- codebase/convolutions.py
+++ codebase/convolutions.py
@@ -42,8 +42,71 @@ def convolve(image, K):
 
     return output
 
+
 def main():
-    print("yolo")
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="path to input image")
+    args = vars(ap.parse_args())
+
+    # construct average blurring kernels to smooth an image
+    smallBlur = np.ones((7, 7), dtype="float") * (1. / (7 * 7))
+    largeBlur = np.ones((21, 21), dtype="float") * (1. / (21 * 21))
+
+    # sharpening filter
+    sharpen = np.array((
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0]), dtype="int")
+    # Laplacian kernel to detect edge-like regions
+    laplacian = np.array((
+        [0, 1, 0],
+        [1, -4, 1],
+        [0, 1, 0]), dtype="int")
+    # Sobel kernel for horizontal edge-like regions
+    sobelX = np.array((
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]), dtype="int")
+    # Sobel kernel for vertical edge-like regions
+    sobelY = np.array((
+        [-1, -2, -1],
+        [0, 0, 0],
+        [1, 2, 1]), dtype="int")
+    # emboss kernel, each pixel of an image is replaced either by a highlight or 
+    # a shadow, depending on light/dark boundaries on the original image
+    emboss = np.array((
+        [-2, -1, 0],
+        [-1, 1, 1],
+        [0, 1, 2]), dtype="int")
+
+    # kernel bank
+    kernelBank = (
+        ("small_blur", smallBlur),
+        ("large_blur", largeBlur),
+        ("sharpen", sharpen),
+        ("laplacian", laplacian),
+        ("sobel_x", sobelX),
+        ("sobel_y", sobelY),
+        ("emboss", emboss)
+        )
+    
+    image = cv2.imread(args["image"])
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # loop over kernels in kernel bank
+    for (kernelName, K) in kernelBank:
+        # apply kernel to grayscale image using both
+        # custom convolve func and OpenCV's filter2D fund
+        print("[INFO] applying {} kernel".format(kernelName))
+        convolveOutput = convolve(gray, K)
+        opencvOutput = cv2.filter2D(gray, -1, K)
+
+        # show images
+        cv2.imshow("original", gray)
+        cv2.imshow("{} - concolve".format(kernelName), convolveOutput)
+        cv2.imshow("{} - opencv".format(kernelName), opencvOutput)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 if __name__=="__main__":
