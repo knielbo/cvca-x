062ada71d28f6492f1682ecb7e181403c190bf34
knielbo <kln@cas.au.dk>
2020-03-04 18:07:01 +0100

    add data import

--- codebase/shallownet_animals.py
+++ codebase/shallownet_animals.py
@@ -16,9 +16,15 @@ import numpy as np
 import argparse
 
 def main():
+    # argument parser and parse arguments
     ap = argparse.ArgumentParser()
     ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
     args = vars(ap.parse_args())
 
+    # grap list of images
+    print("[INFO] loading images...")
+    imagePaths = list(paths.list_images(args["dataset"]))
+    print(imagePaths)
+    
 if __name__=="__main__":
     main()
