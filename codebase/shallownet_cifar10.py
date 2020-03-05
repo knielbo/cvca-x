967f6ddf14ff0734e51bd7358c27d2bac6230ac8
knielbo <kln@cas.au.dk>
2020-03-05 18:46:37 +0100

    add ETL

--- codebase/shallownet_cifar10.py
+++ codebase/shallownet_cifar10.py
@@ -18,6 +18,20 @@ def main():
     gpu_fix()
     os.system("clear")
 
+    # load data and scale [0, 1]
+    print("[INFO] loading CIFAR-10 data...")
+    ((trainX, trainY), (testX, testY)) = cifar10.load_data()
+    trainX = trainX.astype("float") / 255.
+    testX = testX.astype("float") / 255.
+
+    # integers to one-hot vectors
+    lb = LabelBinarizer()
+    trainY = lb.fit_transform(trainY)
+    testY = lb.fit_transform(testY)
+
+    # initialize label names for CIFAR-10 dataset
+    labelNames = ["airplane", "automobile", "bird", "cat", "dear",
+        "dog", "frog", "horse", "ship", "truck"]
 
 
 if __name__=="__main__":
