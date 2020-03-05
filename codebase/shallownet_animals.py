da4c6e07440d47500aaeb0063c30e33014a3698d
knielbo <kln@cas.au.dk>
2020-03-05 18:08:14 +0100

    rm gpu fix

--- codebase/shallownet_animals.py
+++ codebase/shallownet_animals.py
@@ -15,27 +15,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
-import tensorflow as tf
+from kartina.kautil import gpu_fix
+import os
 
 def main():
+    gpu_fix()
+    """
     ###################################################################################
     # bug fix for RTX architecture
         # https://github.com/tensorflow/tensorflow/issues/24496
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+    gpus = tf.config.experimental.list_physical_devices("GPU")
     if gpus:
         try:
             for gpu in gpus:
                 tf.config.experimental.set_memory_growth(gpu, True)
-                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                logical_gpus = tf.config.experimental.list_logical_devices("GPU")
                 print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
             print(e)
     ###################################################################################
-
-    # ANSI escape sequence ##    
-    print("\x1bc")
-    #########################
-
+    """
+    os.system("clear")
     # argument parser and parse arguments
     ap = argparse.ArgumentParser()
     ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
