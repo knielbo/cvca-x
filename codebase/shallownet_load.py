2d0cbb11b706bd186f40618109e5225a2f72495a
knielbo <kln@cas.au.dk>
2020-03-16 12:45:22 +0100

    init loader

---
+++ codebase/shallownet_load.py
@@ -0,0 +1,23 @@
+#!/home/knielbo/virtenvs/cv/bin/python
+"""
+Driver script for application of ShallowNet to animals3
+    - illustrate load methods for trained models
+"""
+from kartina.preprocessing import ImageToArrayPreprocessor
+from kartina.preprocessing import SimplePreprocessor
+from kartina.datasets import SimpleDatasetLoader
+from tensorflow.keras.models import load_model
+from imutils import paths
+import numpy as np
+import argparse
+import cv2
+
+
+
+
+
+def main():
+
+
+if __name__=="__main__":
+    main()
