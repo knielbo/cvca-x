0f6f32719a09d7d55f5dccb8c78bee5658269f9a
knielbo <kln@cas.au.dk>
2020-03-17 17:54:39 +0100

    read and split data

--- codebase/lenet_mnist.py
+++ codebase/lenet_mnist.py
@@ -20,5 +20,20 @@ def main():
     gpu_fix()
     os.system("clear")
 
+    # grab data
+    print("[INFO] accessing MNIST...")
+    ((trainData, trainLabels), (testData, testLabels)) = mnist.load_data()
+
+    # reshape if channels_first for theano users
+        # design matrix is num_samples x depth x rows x columns
+    if K.image_data_format() == "channels_first":
+        trainData = trainData.reshape((trainData.shape[0], 1, 28, 28))
+        testData = testData.reshape((testData.shape[0], 1, 28, 28))
+    
+    # else num_samples x rows x columns x depth
+    else:
+        trainData = trainData.reshape((trainData.shape[0], 28, 28, 1))
+        testData = testData.reshape((testData.shape[0], 28, 28, 1))
+
 if __name__=="__main__":
     main()
